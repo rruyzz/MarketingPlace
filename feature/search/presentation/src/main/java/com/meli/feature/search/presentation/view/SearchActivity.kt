72bed456f2460ad809d84ce3b2e142ab49cf8f4b
rruyzz <rodolforuyz@gmail.com>
2024-09-02 15:35:40 -0300

    adicionado search

--- feature/search/presentation/src/main/java/com/meli/feature/search/presentation/view/SearchActivity.kt
+++ feature/search/presentation/src/main/java/com/meli/feature/search/presentation/view/SearchActivity.kt
@@ -1,11 +1,15 @@
 package com.meli.feature.search.presentation.view
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.google.android.material.search.SearchView
 import com.meli.feature.search.presentation.R
+import com.meli.feature.search.presentation.databinding.ActivitySearchBinding
 import com.meli.feature.search.presentation.viewmodel.SearchViewModel
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -13,18 +17,39 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class SearchActivity : AppCompatActivity() {
 
     private val viewModel by viewModel<SearchViewModel>()
+    private val binding: ActivitySearchBinding by lazy {
+        ActivitySearchBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_search)
+        setContentView(binding.root)
+        setButtons()
+        searchBar()
         actionObserver()
     }
 
     private fun actionObserver() = lifecycleScope.launch {
-        val viewText = findViewById<TextView>(R.id.textIDearchd)
         viewModel.castMoviesState.collect { state ->
-            viewText.text = state
+            binding.text1.text = state
+        }
+    }
+
+    private fun searchBar() {
+        binding.apply {
+            searchView.setupWithSearchBar(searchBar)
+        }
+    }
+    private fun setButtons() {
+        binding.apply {
+            searchView.editText.setOnEditorActionListener { textView, i, keyEvent ->
+                val queryText = textView.text.toString()
+                searchBar.setText(queryText)
+                Toast.makeText(this@SearchActivity, queryText, Toast.LENGTH_SHORT).show()
+                searchView.hide()
+                return@setOnEditorActionListener false
+            }
         }
     }
 }
