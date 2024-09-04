661b5be05eeebe643ad83453b6055487b34f80d5
rruyzz <rodolforuyz@gmail.com>
2024-09-03 21:19:26 -0300

    money format

--- core/network/src/main/java/com/meli/core/network/domain/products/mapper/ProductMapper.kt
+++ core/network/src/main/java/com/meli/core/network/domain/products/mapper/ProductMapper.kt
@@ -2,12 +2,21 @@ package com.meli.core.network.domain.products.mapper
 
 import com.meli.core.network.data.model.ProductSearchResponse
 import com.meli.core.network.domain.products.model.ProductDto
+import java.text.NumberFormat
+import java.util.Currency
 
 fun ProductSearchResponse.toDomain() = this.results?.map { result ->
     ProductDto(
         id = result?.id.orEmpty(),
         title = result?.title.orEmpty(),
-        price = result?.price.toString(),
+        price = result?.price.toMoneyFormat(),
         thumbnail = result?.thumbnail.orEmpty()
     )
 } ?: listOf()
+
+fun Double?.toMoneyFormat(): String {
+    val format = NumberFormat.getCurrencyInstance()
+    format.setMaximumFractionDigits(0);
+    format.setCurrency(Currency.getInstance("BRL"));
+    return format.format(this)
+}
